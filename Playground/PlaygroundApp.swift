326c77ebe574b36f50c0012398b402207813aad0
Antonymous1337 <45654017+Antonymous1337@users.noreply.github.com>
2025-01-02 04:10:03 -0800

    Initial Commit

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Antony Holshouser on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
