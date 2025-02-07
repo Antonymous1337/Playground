509f1dfcb8fe81fedbaca6736a10ee119d09a794
Antonymous1337 <45654017+Antonymous1337@users.noreply.github.com>
2025-02-07 15:33:24 -0800

    Tried to play audio when app is closed

---
+++ Playground/ClosedAppAudioPractice.swift
@@ -0,0 +1,18 @@
+//
+//  ClosedAppAudioPractice.swift
+//  Playground
+//
+//  Created by Antony Holshouser on 2/7/25.
+//
+
+import SwiftUI
+
+struct ClosedAppAudioPractice: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ClosedAppAudioPractice()
+}
---
+++ Playground/MutedColorPractice.swift
@@ -0,0 +1,28 @@
+//
+//  MutedColorPractice.swift
+//  Playground
+//
+//  Created by Antony Holshouser on 1/23/25.
+//
+
+import SwiftUI
+
+struct MutedColorPractice: View {
+    var body: some View {
+        ZStack {
+            Text("Hello, World")
+                .frame(maxWidth: .infinity)
+                .padding()
+                .background {
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(Color.mutedYellow)
+                }
+                .padding()
+        }
+    }
+}
+
+#Preview {
+        MutedColorPractice()
+    
+}
